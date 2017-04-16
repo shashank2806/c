aa08e73c6b2d42cfd471ee7727bd280a497a081a
Shashank Shekhar <shashank.shekhar28june@gmail.com>
2017-04-17 04:45:00 +0530

    removed unnecessary lines

--- prime.c
+++ prime.c
@@ -3,16 +3,14 @@
 
 int main()
 {
-	int x=0,y =0,n,i;
+	int x = 0,n,i;
 	printf("Enter the no.: ");
 	scanf("%i",&n);
 	for(i=2;i<n;i++)
 	{
 		if (n%i==0)
 			x++;
-		//x will only be incremented if no is not prime.
-		else
-			y++;	
+		//x will only be incremented if no is not prime.	
 	}
 	if(x>0)
 		printf("Not Prime\n");
