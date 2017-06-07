306072184d555f3fd26d34616de412afb19d1b0a
Shashank Shekhar <shashank.shekhar28june@gmail.com>
2017-06-07 12:33:52 +0530

    god knows what am i doing

--- palindrome.c
+++ palindrome.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	int n,original,reverse=0,rem;
+	int n,original,reverse=0,rem,x=100;
 	printf("Enter the no.\n");
 	scanf("%i",&n);
 	original = n;
@@ -10,7 +10,9 @@ int main(void)
 	while(n!=0)
 	{
 		rem = n%10;
-		
+		n=n/10;
+		reverse=reverse*10+rem;
+		x/=10;
 	}
-
+	printf("%i\n",reverse );
 }
